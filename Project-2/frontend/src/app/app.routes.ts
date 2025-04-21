ec27bff5c5de793598be02cad14ad1dad52cd52f
Hiren Khunt <hirenkhunt101@gmail.com>
2025-04-21 04:07:10 +0000

    pagination practise

---
+++ Project-2/frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PaginationComponent } from '../pages/pagination/pagination.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: PaginationComponent
+    }
+];
---
+++ Project-2/frontend/src/pages/pagination/pagination.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.css'
+})
+export class PaginationComponent {
+
+  productData: any;
+  responseData: any;
+  pageSize = 10;
+  totalCount = 194;
+  currentPage = 1;
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    let data: any = await fetch('https://dummyjson.com/products?limit=184');
+    data = await data.json();
+    this.responseData = data.products;
+    this.getData(1);
+    console.log(this.responseData);
+
+  }
+
+  getData(pageNumber: any) {
+    this.currentPage = pageNumber
+    let start = (pageNumber - 1) * this.pageSize;
+    this.productData = [];
+
+    for (let i = start; i < start + this.pageSize; i++) {
+      this.productData.push(this.responseData[i]);
+    }
+  }
+
+  get pages(): number[] {
+    return Array.from({ length: this.totalCount / this.pageSize }, (_, i) => i + 1);
+  }
+
+}
